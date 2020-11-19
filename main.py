a1262cc1f44317c7d17823b39cdda0b1e198ef92
jetwardenier <jet.wardenier@gmail.com>
2020-11-19 11:25:27 +0100

    x

--- main.py
+++ main.py
@@ -3,28 +3,41 @@ from Classes.Group import Group
 from Classes.Lift import Lift
 import simpy
 
+# We definieren het totaal aantal verdiepingen in de simulatie
 totalFloors = 6 + 1
+# GroundLevel is de begane grond in onze simulatie, deze stellen we gelijk aan 1
 groundLevel = 1
+
 class EV:
 
     # def __init__(self, env):
     #     self.env = env
     #     self.lift_proc = env.process(self.liftSim(env))
 
-
+    # Met populate floors zetten we een bepaald aantal mensen op een bepaalde verdieping.
+    # deze mensen willen gebruik gaan maken van de lift.
     def populateFloors(self):
         floors = {}
         for floorNr in range(groundLevel, totalFloors):
+            # We populeren elke verdieping van de simulatie met een groep van 10 personen.
             floors.update({floorNr :Group(10, floorNr, totalFloors)})
         return floors
 
-
+    # Hieronder definieren we onze lift simulatie functie.
     def liftSim(self):
+        # We willen de duratie van het vervoeren van een X aantal personen berekenen.
+        # We stellen eerst de totale tijd gelijk aan 0.
         totalTime = 0
+        # We creeëren een instantie van lift.
         lift = Lift('Lift1',groundLevel,totalFloors)
+        # We roepen de functie populateFloors aan.
         floors = self.populateFloors()
         hasMoves, time = lift.move()
+        # De lift gaat bewegen en we tellen de tijd dat dit kost op bij de totale tijd.
         totalTime += time
+        # In de while loop stappen eerst de personen uit die op hun destination zijn.
+        # Daarna wordt de lift weer gevuld met mensen die op de huidige verdieping staan en
+        # een andere destination hebben
         while hasMoves:
             floors[lift.curr_floor].putArrivals(lift.empty())
 
