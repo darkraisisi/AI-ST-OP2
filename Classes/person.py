7b7d7ce4c49ce8c3b2bfbe17a563bbf3c4950098
David Demmers <daviddemmers@gmail.com>
2020-12-02 17:34:10 +0100

    Test for plurality voting

--- Classes/person.py
+++ Classes/person.py
@@ -1,19 +1,35 @@
 from random import uniform
+from abc import ABC, abstractmethod
+import numpy as np
+
 class Person:
     """
         Main class that houses most of a persons logic, like political views 
     """
-    def __init__(self, views=None):
-        if views:
+    def __init__(self, position=None):
+        if position:
             # values between -1 & 1, 0 is exactly in the middle of the spectrum.
-            self.views = views 
+            self.position = position 
         else:
-            self.views = self.generateViews()
+            self.position = self.generatePosition()
 
     
-    def generateViews(self) -> [float, float]:
-        return uniform(-1, 1), uniform(-1, 1)
+    def generatePosition(self) -> [float, float]:
+        return np.array([uniform(-1, 1), uniform(-1, 1)])
 
+
+class Candidate(Person):
+
+    def __init__(self):
+        super().__init__()
+        self.amountVotes = 0
+
+
+    def addVotes(self, n:int) -> int:
+        self.amountVotes += n
+        return self.amountVotes
+
+    
 
 class Voter(Person):
 
@@ -25,10 +41,31 @@ class Voter(Person):
         pass
 
 
-
-class Candidate(Person):
-
+class HonestVoter(Voter):
     def __init__(self):
         super().__init__()
+    
+    @abstractmethod
+    def castVote(self,candidates:list):
+        finalCandidate = None
+        runnerUp = 100
+
+        for i, cand in enumerate(candidates,0):
+            distance = np.linalg.norm(self.position - cand.position)
+
+            if distance < runnerUp:
+                finalCandidate = cand
+                runnerUp = distance
+
+        finalCandidate.addVotes(1)
 
 
+
+
+class StrategicVoter(Voter):
+    def __init__(self):
+        super().__init__()
+    
+    @abstractmethod
+    def castVote(self):
+        pass
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.modules import ChartModule
 from mesa.visualization.UserParam import UserSettableParameter
 
-from Classes.Person import Person, Voter, Candidate
+from Classes.Person import Person, Voter, HonestVoter, StrategicVoter, Candidate
 from Classes.Model import VoterModel
 
 if __name__ == "__main__":
@@ -20,3 +20,21 @@ if __name__ == "__main__":
     server.port = 8521
     server.launch()
 
+    # Test of plurality voting
+    candidates = []
+    voters = []
+    for i in range(3): # Get some candidates
+        candidates.append(Candidate())
+    for i in range(10000): # Get some voters
+        voters.append(HonestVoter())
+
+    for vot in voters: # Make all the voters vote
+        # print(vot.position)
+        vot.castVote(candidates)
+
+    total = 0
+    for i, can in enumerate(candidates): # See the final score and actual candidate position
+        print(i,can.amountVotes,can.position)
+        total += can.amountVotes
+
+    print(total)
