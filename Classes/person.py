fcf726c3efc382f3f7259f0e9800e03237278df3
AmaryllisLee <ama.lee@student.hu.nl>
2020-12-06 19:05:09 +0100

    code for StrategicVoter (plurality) and update abstract methods

--- Classes/person.py
+++ Classes/person.py
@@ -38,10 +38,11 @@ class Voter(Person):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
     
-
+    def distanceCandidates(self): # calculating the distance between the voter and the candidate
+        pass
     def castVote(self):
         pass
-
+    
     def step(self):
         pass
 
@@ -49,15 +50,22 @@ class Voter(Person):
 class HonestVoter(Voter):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+
+    @abstractmethod
+    def distanceCandidates(self, candidates:list):
+        'Calculating the distance between voters and candidate'
+        distance =  {}
+
+        for _, cand in enumerate(candidates,0):
+            distance[cand] = np.linalg.norm(self.position - cand.position)
+        return distance
     
     @abstractmethod
-    def castVote(self,candidates:list):
+    def castVote(self,distCand:dict):
         finalCandidate = None
-        runnerUp = 100
+        runnerUp =100
 
-        for i, cand in enumerate(candidates,0):
-            distance = np.linalg.norm(self.position - cand.position)
-
+        for cand, distance in distCand.items:
             if distance < runnerUp:
                 finalCandidate = cand
                 runnerUp = distance
@@ -75,6 +83,38 @@ class StrategicVoter(Voter):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
     
+    
     @abstractmethod
-    def castVote(self):
-        pass
+    def distanceCandidates(self, candidates:list):
+        'Calculating the distance between voters and candidate'
+        distance =  {}
+
+        for _, cand in enumerate(candidates,0):
+            distance[cand] = np.linalg.norm(self.position - cand.position)
+        return distance
+
+    @abstractmethod
+    def castVote(self, distCand:dict, resultPoll:dict): #TODO define function in Model.py to get the result of a poll
+        finalCandidate = None
+        runnerUp =  0
+
+        for i in resultPoll.keys:
+            if resultPoll.get(i) > runnerUp:
+                finalCandidate = i
+                runnerUp = resultPoll.get(i)
+
+        #now that we have the Candidate with highest chance of winning, we want to also consider the distance between voter and candidates.
+        results =  sorted(resultPoll.values())
+        for i in distCand.keys:
+            if distCand.get(i) < distCand.get(finalCandidate):
+                if results.index(resultPoll.get(i))!= -1:
+                    finalCandidate = i
+                    runnerUp = resultPoll.get(i)
+        
+        finalCandidate.addVotes(1) # cast vote 
+
+
+
+    @abstractmethod
+    def step(self):
+        self.castVote(model.candidates)
