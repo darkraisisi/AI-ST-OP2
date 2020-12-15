249c9a9244164276c624557fc4f36c4a9c225edd
David Demmers <daviddemmers@gmail.com>
2020-12-15 21:07:30 +0100

    resolving merge error

--- Classes/person.py
+++ Classes/person.py
@@ -1,6 +1,7 @@
 from random import uniform
 from abc import ABC, abstractmethod
 import numpy as np
+import random
 from mesa import Agent, Model
 
 class Person(Agent):
@@ -126,18 +127,18 @@ class StrategicVoter(Voter):
         distCand: dictionary that contains the distance between the voters and ccandidates
         resultPoll: the amount of Votes of each candidate of the previous poll
         """
-        distCand = sorted(distCand.items(), key=lambda x: x[1]) # sort Candidates
-        finalCandidate = distCand[0] # the canndidate who the voter is goin to vote for. Initial value will be the candidate wih the shortest distance
-        runnerUp = distCand[1] 
-        
-        #Check whether finalCandidate has a better chance of winningg than runnerUp
-        # TODO :  This startegy will only be applied too two candidates. What happens to the rest?
-        if resultPoll.get(finalCandidate[0]) < resultPoll.get(runnerUp[0]):
-            diff = resultPoll.get(runnerUp[0]) - resultPoll.get(finalCandidate[0])
-            #Calculate how better of chance you have if you choose runner up
-            if diff / resultPoll.get(runnerUp[0]) > self.model.loyalty: 
-                #We vote for that candidate
-                finalCandidate = runnerUp
+        distCand = sorted(distCand.items(), key=lambda x: x[1])
+        finalCandidate = distCand[0]
+        runnerUp = distCand[1]
+
+        if random.randint(0,100) <= self.model.strat_chance and self.model.currentPollCounter != 0:
+            #now that we have the Candidate with highest chance of winning, we want to also consider the distance between voter and candidates.
+            if resultPoll.get(finalCandidate[0]) < resultPoll.get(runnerUp[0]):
+                diff = resultPoll.get(runnerUp[0]) - resultPoll.get(finalCandidate[0])
+                if diff / resultPoll.get(runnerUp[0]) > self.model.loyalty: 
+                    #We vote for that candidate
+                    self.model.strat_counter += 1
+                    finalCandidate = runnerUp
 
         self.color = finalCandidate[0].color
         return finalCandidate[0]
